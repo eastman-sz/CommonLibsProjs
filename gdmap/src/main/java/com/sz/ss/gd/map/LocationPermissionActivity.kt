9b6933a6ee1aab13ba18fa57553486c3e9b05f7a
258474063@qq.com <Quiet800>
2018-08-16 17:27:42 +0800

    update gd map

--- gdmap/src/main/java/com/sz/ss/gd/map/LocationPermissionActivity.kt
+++ gdmap/src/main/java/com/sz/ss/gd/map/LocationPermissionActivity.kt
@@ -1,7 +1,12 @@
 package com.sz.ss.gd.map
 
+import android.Manifest
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
 /**
  * Check permission.
@@ -45,6 +50,26 @@ class LocationPermissionActivity : Activity() {
 
 
                     false ->{
+
+                        //点击了拒绝，并不再弹出权限窗口时
+                        try {
+                            val asked =  shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)
+
+                            if (!asked){
+                                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                val uri = Uri.fromParts("package", packageName, null)
+                                intent.data = uri
+                                startActivity(intent)
+
+                                return
+                            }
+                        }catch (e : Exception){
+                            finish()
+
+                            GdConfig.onLocationPermissionRequesterListener?.onDeny()
+                        }
+
+
                         Toast.makeText(this , "没有权限将不能使用定位服务" , Toast.LENGTH_SHORT).show()
 
                         finish()
@@ -59,6 +84,25 @@ class LocationPermissionActivity : Activity() {
 
     }
 
+    override fun shouldShowRequestPermissionRationale(permission: String?): Boolean {
+        return super.shouldShowRequestPermissionRationale(permission)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        finish()
+
+        val hasLocationPermission = LocationPermissionHelper.hasLocationPermission(this)
+        when(hasLocationPermission){
+            true ->{
+                GdConfig.onLocationPermissionRequesterListener?.onGranted()
+            }
+
+            false -> {
+                GdConfig.onLocationPermissionRequesterListener?.onDeny()
+            }
+        }
+    }
 
 
 }
